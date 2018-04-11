84d9ccdb0cdef8248397afdfb510eeb62f8cd599
Bruno DaSilva <33585326+DaSilvaBrunoTexas@users.noreply.github.com>
2018-04-10 23:26:59 -0500

    Add files via upload

---
+++ frmScoreCalculator.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace ScoreCalculator
+{
+    partial class frmScoreCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmScoreCalculator));
+            this.lblScore = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.lblAverage = new System.Windows.Forms.Label();
+            this.txtScore = new System.Windows.Forms.TextBox();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.txtCount = new System.Windows.Forms.TextBox();
+            this.txtAverage = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.picLogo = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.ForeColor = System.Drawing.Color.White;
+            this.lblScore.Location = new System.Drawing.Point(176, 18);
+            this.lblScore.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(83, 29);
+            this.lblScore.TabIndex = 0;
+            this.lblScore.Text = "Score:";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.ForeColor = System.Drawing.Color.White;
+            this.lblTotal.Location = new System.Drawing.Point(152, 47);
+            this.lblTotal.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(144, 29);
+            this.lblTotal.TabIndex = 1;
+            this.lblTotal.Text = "Score Total:";
+            // 
+            // lblCount
+            // 
+            this.lblCount.AutoSize = true;
+            this.lblCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCount.ForeColor = System.Drawing.Color.White;
+            this.lblCount.Location = new System.Drawing.Point(148, 76);
+            this.lblCount.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(152, 29);
+            this.lblCount.TabIndex = 2;
+            this.lblCount.Text = "Score Count:";
+            // 
+            // lblAverage
+            // 
+            this.lblAverage.AutoSize = true;
+            this.lblAverage.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAverage.ForeColor = System.Drawing.Color.White;
+            this.lblAverage.Location = new System.Drawing.Point(167, 105);
+            this.lblAverage.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblAverage.Name = "lblAverage";
+            this.lblAverage.Size = new System.Drawing.Size(108, 29);
+            this.lblAverage.TabIndex = 3;
+            this.lblAverage.Text = "Average:";
+            // 
+            // txtScore
+            // 
+            this.txtScore.BackColor = System.Drawing.Color.DimGray;
+            this.txtScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtScore.ForeColor = System.Drawing.Color.White;
+            this.txtScore.Location = new System.Drawing.Point(330, 18);
+            this.txtScore.Margin = new System.Windows.Forms.Padding(2);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(211, 30);
+            this.txtScore.TabIndex = 4;
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.BackColor = System.Drawing.Color.DimGray;
+            this.txtTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTotal.ForeColor = System.Drawing.Color.White;
+            this.txtTotal.Location = new System.Drawing.Point(330, 48);
+            this.txtTotal.Margin = new System.Windows.Forms.Padding(2);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(211, 30);
+            this.txtTotal.TabIndex = 5;
+            // 
+            // txtCount
+            // 
+            this.txtCount.BackColor = System.Drawing.Color.DimGray;
+            this.txtCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCount.ForeColor = System.Drawing.Color.White;
+            this.txtCount.Location = new System.Drawing.Point(330, 78);
+            this.txtCount.Margin = new System.Windows.Forms.Padding(2);
+            this.txtCount.Name = "txtCount";
+            this.txtCount.Size = new System.Drawing.Size(211, 30);
+            this.txtCount.TabIndex = 6;
+            // 
+            // txtAverage
+            // 
+            this.txtAverage.BackColor = System.Drawing.Color.DimGray;
+            this.txtAverage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAverage.ForeColor = System.Drawing.Color.White;
+            this.txtAverage.Location = new System.Drawing.Point(330, 108);
+            this.txtAverage.Margin = new System.Windows.Forms.Padding(2);
+            this.txtAverage.Name = "txtAverage";
+            this.txtAverage.Size = new System.Drawing.Size(211, 30);
+            this.txtAverage.TabIndex = 7;
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.ForeColor = System.Drawing.Color.White;
+            this.btnClear.Location = new System.Drawing.Point(254, 169);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(2);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(180, 38);
+            this.btnClear.TabIndex = 8;
+            this.btnClear.Text = "Clear Scores";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.ForeColor = System.Drawing.Color.White;
+            this.btnExit.Location = new System.Drawing.Point(438, 169);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(2);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(180, 38);
+            this.btnExit.TabIndex = 9;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.ForeColor = System.Drawing.Color.White;
+            this.btnAdd.Location = new System.Drawing.Point(70, 169);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(2);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(180, 38);
+            this.btnAdd.TabIndex = 10;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // picLogo
+            // 
+            this.picLogo.Image = global::ScoreCalculator.Properties.Resources.BrunoWebDesign;
+            this.picLogo.Location = new System.Drawing.Point(12, 12);
+            this.picLogo.Name = "picLogo";
+            this.picLogo.Size = new System.Drawing.Size(131, 78);
+            this.picLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picLogo.TabIndex = 11;
+            this.picLogo.TabStop = false;
+            this.picLogo.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // frmScoreCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(689, 230);
+            this.Controls.Add(this.picLogo);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.txtAverage);
+            this.Controls.Add(this.txtCount);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.lblAverage);
+            this.Controls.Add(this.lblCount);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lblScore);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "frmScoreCalculator";
+            this.Text = "Score Calculator";
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label lblCount;
+        private System.Windows.Forms.Label lblAverage;
+        private System.Windows.Forms.TextBox txtScore;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.TextBox txtCount;
+        private System.Windows.Forms.TextBox txtAverage;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.PictureBox picLogo;
+    }
+}
+
---
+++ frmScoreCalculator.cs
@@ -0,0 +1,83 @@
+﻿/*
+ *Author: Bruno Leandro DaSilva 
+ *Course: ITSE-1330.WW1: Introduction to C# Programming  
+ * Date: 09/08/2017
+ * Application: Score Calculator - Project 1
+ * Description: This Application Adds Score values and displayes the Total Score, Count,
+ * and average of a given set of scores. Additionally, the user can clear the scores 
+ * entered and Exit the application.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScoreCalculator
+{
+    public partial class frmScoreCalculator : Form
+    {
+        private List<int> Scores = new List<int>();
+        private int total = 0;
+        private float average = 0F;
+
+        public frmScoreCalculator()
+        {
+            InitializeComponent();
+        }
+
+        private float CalculateAverage(int sum, int n)
+        {
+            return (float)sum / (float)n; 
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            
+            int score = Convert.ToInt32(txtScore.Text);
+
+            Scores.Add(score);
+
+           
+            total += score;
+            txtTotal.Text = total.ToString();
+
+            txtCount.Text = Scores.Count.ToString();
+
+            average = CalculateAverage(total, Scores.Count);
+            txtAverage.Text = average.ToString("0.0");
+
+            txtScore.Text = string.Empty;
+            txtScore.Focus();
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            Scores.Clear();
+            total = 0;
+            average = 0F;
+
+            txtTotal.Text = total.ToString();
+            txtCount.Text = Scores.Count.ToString();
+            txtAverage.Text = average.ToString();
+            txtScore.Focus();
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
